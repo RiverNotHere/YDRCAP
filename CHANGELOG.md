01a5ec00bbed282b8f8395d7dd977cd3fb208e22
o0River0o <gyoushijie@gmail.com>
2021-02-13 20:38:48 -0800

    Updated 2021-02-13
    Signed-off-by: o0River0o <gyoushijie@gmail.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,5 +4,35 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [YDRC Admin Portal](https://github.com/o0River0o/YDRCAP).
 
+## [Unreleased]
+- Events can now be edited
+
 ## [1.0.0] 2020-11-06 11:03 AM
 - Initial Release
+
+## [1.0.1] 2020-12-06 12:01 AM
+### Changed
+- Optimization in UI of the admin dashboard
+
+### Fixed
+- Common Bug Fixed
+
+## [1.0.2] 2020-12-25 11:00 AM
+### Changed
+- Auto calculated volunteer time live when loading the "Manage Account" page
+
+## [1.0.3] 2021-01-06 12:10 AM
+### Changed
+- Time optimization in "Manage Account" page
+- Algorithm optimization for calculation
+- Optimized throughput 
+
+### Fixed
+- Bug Fixed
+
+## [1.1.0] 2021-02-06 11:41 AM
+### Added
+- Event type of the record can now be changed by administrator and coordinators
+
+### Changed
+- Record loading time optimized (100ms)
--- app.js
+++ app.js
@@ -5,21 +5,30 @@
  *
  */
 
+/*=================================
+*                                 *
+*   Project: YDRCAP(Admin Portal) *
+* ------------------------------- *
+*    Created at: 2020-10-10       *
+*    Last Changed: 2021-02-13     *
+*                                 *
+==================================*/
+
 /* Modules */
-const path = require('path')
+const path = require('path');
 require('dotenv').config({ path: './config/config.env' });
 
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-const exphbs = require('express-handlebars')
+const exphbs = require('express-handlebars');
 
 //Moment
-const moment = require('moment')
+const moment = require('moment');
 
 //Express Sessions
-const flash = require('connect-flash')
-const session = require('express-session')
+const flash = require('connect-flash');
+const session = require('express-session');
 app.use(session({
   secret: 'secret',
   resave: true,
@@ -27,32 +36,32 @@ app.use(session({
 }))
 app.use(flash());
 
-const connectDB = require('./config/db')
-connectDB()
+const connectDB = require('./config/db');
+connectDB();
 
-const passport = require('passport')
+const passport = require('passport');
 
 // Passport Config
-require('./config/passport')(passport)
+require('./config/passport')(passport);
 
 // Passport middleware
-app.use(passport.initialize())
-app.use(passport.session())
+app.use(passport.initialize());
+app.use(passport.session());
 
 //Global vars
 app.use((req, res, next) => {
-  res.locals.success_msg = req.flash('success_msg')
-  res.locals.error_msg = req.flash('error_msg')
-  res.locals.error = req.flash('error')
-  next()
-})
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+});
 
 // Utils
-const { isSelected, newSelectGroup } = require('./utils/recInfo')
+const { isSelected, newSelectGroup } = require('./utils/recInfo');
 
 // Models
-const Records = require('./models/Record')
-const Config = require('./models/Config')
+const Records = require('./models/Record');
+const Config = require('./models/Config');
 
 // Handlebars Config
 app.engine('.hbs', exphbs({
@@ -63,43 +72,41 @@ app.engine('.hbs', exphbs({
   helpers: {
     // Format Date
     formatDate: (datetime, format) => {
-      return moment(datetime).format(format)
+      return moment(datetime).format(format);
     },
 
     // Get Status
     switch: (status) => {
       switch (status) {
         case 0:
-          return '<font color="red">Unconfirmed</font>'
-          break
+          return '<font color="red">Unconfirmed</font>';
         case 1:
-          return '<font color="green">Confirmed</font>'
-          break
+          return '<font color="green">Confirmed</font>';
         default:
-          return null
+          return null;
       }
     },
 
     // Get sum
     sum: (a, b) => {
-      return a + b
+      return a + b;
     },
 
     // Calculating the increase/decreases
     getStat: (now, before, period) => {
-      let decreaseValue = before - now
-      let perc = (decreaseValue / before) * 100
-      console.log(perc)
-      console.log(perc > 0)
+      let decreaseValue = before - now;
+      let perc = (decreaseValue / before) * 100;
+      console.log(perc);
+      console.log(perc > 0);
       if(period == 'day') {
-        period = 'yesterday'
+        period = 'yesterday';
       } else {
-        period = "last " + period
+        period = "last " + period;
       }
       if (perc > 0) {
-        return `<font color="red"><i class="material-icons">arrow_downward</i> ${Math.abs(+parseFloat(perc.toFixed(2)))}% since ${period}</font>`
+        return `<font color="red"><i class="material-icons">arrow_downward</i> ${Math.abs(+parseFloat(perc.toFixed(2)))}% since ${period}</font>`;
       }else {
-        return `<font color="green"><i class="material-icons">arrow_upward</i> ${Math.abs(+parseFloat(perc.toFixed(2)))}% since ${period}</font>`
+        return `<font color="green"><i class="material-icons">arrow_upward</i> ${Math.abs(+parseFloat(perc.toFixed(2)))}% since ${period}</font>`;
       }
     },
 
@@ -107,63 +114,63 @@ app.engine('.hbs', exphbs({
     // ${status}: 0 || 1 (unapproved/approved)
     isshow: (status) => {
       if (status == 0) {
-        return ''
+        return '';
       } else {
-        return 'hide'
+        return 'hide';
       }
     },
 
     // Get Age Group
     // Rule from PVSA
     getAgeGroup: (year, month) => {
-      let dob = new Date(year, month)
-      let diff_ms = Date.now() - dob.getTime()
-      let age_dt = new Date(diff_ms)
-      let age = Math.abs(age_dt.getUTCFullYear() - 1970)
+      let dob = new Date(year, month);
+      let diff_ms = Date.now() - dob.getTime();
+      let age_dt = new Date(diff_ms);
+      let age = Math.abs(age_dt.getUTCFullYear() - 1970);
 
       switch (true) {
         case (age >= 5 && age <= 10):
-          return "Kids"
+          return "Kids";
         case (age >= 11 && age <= 15):
-          return "Teens"
+          return "Teens";
         case (age >= 16 && age <= 25):
-          return "Young Adults"
+          return "Young Adults";
         case (age >= 26):
-          return "Adults"
+          return "Adults";
         default:
-          return "Invalid Age"
+          return "Invalid Age";
       }
     },
   }
 }));
-app.set('view engine', '.hbs')
+app.set('view engine', '.hbs');
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
 //DB Models
-const User = require('./models/User')
+const User = require('./models/User');
 
 //Body Parser
-app.use(express.urlencoded({ extended: false }))
-app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 //Routes
-app.use('/', require('./routes/user'))
-app.use('/a/', require('./routes/portal'))
-app.use('/u/', require('./routes/account'))
+app.use('/', require('./routes/user'));
+app.use('/a/', require('./routes/portal'));
+app.use('/u/', require('./routes/account'));
 
 //Error Handling
 // Handle 404
 app.use(function (req, res) {
-  res.render('./errors/404')
+  res.render('./errors/404');
 });
 
 // Handle 500
 app.use(function (error, req, res, next) {
-  res.render('./errors/500')
+  res.render('./errors/500');
 });
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Site is running on ${PORT}`)
+  console.log(`Site is running on ${PORT}`);
 })
